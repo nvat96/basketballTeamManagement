a5947491df1bfd2a19d3f463f4c8ebb308172232
Tu Nguyen <nvat96@gmail.com>
2022-06-27 07:06:02 +0700

    Try to add exceptions

--- src/main/java/com/axonactive/basketball/services/ArenaService.java
+++ src/main/java/com/axonactive/basketball/services/ArenaService.java
@@ -1,15 +1,15 @@
 package com.axonactive.basketball.services;
 
-import com.axonactive.basketball.apis.requests.ArenaRequest;
 import com.axonactive.basketball.entities.Arena;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ArenaService {
     List<Arena> findAll();
-    Arena findByID(String name);
+    Optional<Arena> findByID(String arenaName);
     Arena save(Arena arena);
-    void deleteByID(String name);
+    void deleteByID(String arenaName);
     Arena create(Arena arena);
-    Arena update(String name, ArenaRequest arenaRequest);
+    Arena update(String arenaName, Arena arena);
 }
--- src/main/java/com/axonactive/basketball/services/CoachService.java
+++ src/main/java/com/axonactive/basketball/services/CoachService.java
@@ -1,5 +1,6 @@
 package com.axonactive.basketball.services;
 
+import com.axonactive.basketball.apis.requests.CoachRequest;
 import com.axonactive.basketball.entities.Coach;
 import org.springframework.data.repository.query.Param;
 
@@ -14,5 +15,7 @@ public interface CoachService {
     Optional<Coach> findByFirstNameAndLastName(String firstName, String lastName);
     List<Coach> findByFirstNameLike(String firstName);
     List<Coach> findByFirstNameLikeAndLastNameLike(String firstName, String lastName);
+    Coach create(CoachRequest coachRequest);
+    Coach update(Integer id, CoachRequest coachRequest);
 
 }
